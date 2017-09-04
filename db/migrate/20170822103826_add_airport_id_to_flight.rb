9f492a0857ff21c4e15e65b37b7333b83403db0e
Helenga <burlena23@gmail.com>
2017-09-05 00:23:40 +0300

    Change migration

--- db/migrate/20170822103826_add_airport_id_to_flight.rb
+++ db/migrate/20170822103826_add_airport_id_to_flight.rb
@@ -1,8 +1,8 @@
 class AddAirportIdToFlight < ActiveRecord::Migration[5.1]
   def change
 	add_foreign_key :flights, :airports, 
-			column: :from_airport_id
+			column: :from_airport_id, primary_key: :id
 	add_foreign_key :flights, :airports, 
-			column: :to_airport_id
+			column: :to_airport_id, primary_key: :id
   end
 end
