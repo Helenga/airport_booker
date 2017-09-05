e271589db02701301a2182c45f8c444d6e1a4ee6
Helenga <burlena23@gmail.com>
2017-09-05 17:57:05 +0300

    inject in airport model inverse_of

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,6 +1,6 @@
 class Airport < ApplicationRecord
 	has_many :departing_flights, class_name: 'Flight', 
-				foreign_key: :from_airport_id
+			foreign_key: :from_airport_id, inverse_of: 'from_airport'
 	has_many :arriving_flights, class_name: 'Flight', 
-				foreign_key: :to_airport_id
+			foreign_key: :to_airport_id, inverse_of: 'to_airport'
 end
